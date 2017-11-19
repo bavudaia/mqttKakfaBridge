56a99f59c903934b66cb2db653b02d8051b4e049
bavudaia <bala.murugan.nitt@gmail.com>
2017-11-18 19:25:06 -0800

    version changes

--- src/main/java/com/m2mci/mqttKafkaBridge/Bridge.java
+++ src/main/java/com/m2mci/mqttKafkaBridge/Bridge.java
@@ -32,8 +32,8 @@ public class Bridge implements MqttCallback {
 		 props.put("batch.size", 16384);
 		 props.put("linger.ms", 1);
 		 props.put("buffer.memory", 33554432);
-		 //props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		 props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		 props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		 props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
 		kafkaProducer = new KafkaProducer<String, Message>(props);
 		token.waitForCompletion();
 		logger.info("Connected to MQTT and Kafka");
